ca66390d3afbeda2d33118e77c3002228d65bb63
harry hirsch <harry_hirsch@protonmail.com>
2024-04-23 20:21:04 +0200

    u

---
+++ ClsFusionViewer/ViewModels/ClsLogFile.cs
@@ -0,0 +1,34 @@
+﻿using InoTec;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClsFusionViewer.ViewModels
+{
+    public class ClsLogFile
+    {
+        private string _year;
+        private string _month;
+        private ObservableCollection<ClsLogFileLine> _logFiles;
+
+        public string Year => _year;
+        public string Month => _month;
+        public ObservableCollection<ClsLogFileLine> LogFiles => _logFiles;
+
+        public ClsLogFile(IEnumerable<ClsLogFileLineType> model)
+        {
+            _logFiles = new ObservableCollection<ClsLogFileLine>(model.Select(x => new ClsLogFileLine(x)));
+            _year = _logFiles[0].Year;
+            _month = _logFiles[0].Month;
+        }
+        public ClsLogFile(string s, IEnumerable<ClsLogFileLine> lines)
+        {
+            _year = s;
+            _month = string.Empty;
+            _logFiles = new ObservableCollection<ClsLogFileLine>(lines);
+        }
+    }
+}
--- ClsFusionViewer/ViewModels/ClsLogViewModel.cs
+++ ClsFusionViewer/ViewModels/ClsLogViewModel.cs
@@ -5,33 +5,182 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security.RightsManagement;
+using System.Windows.Forms;
+using System.Windows.Shapes;
 
 namespace ClsFusionViewer.ViewModels
 {
     public class ClsLogViewModel : BaseLogViewModel
     {
-        private ObservableCollection<ObservableCollection<ClsLogFileLine>> _clsLogFiles;
-        public ObservableCollection<ClsLogFileLine> ClsLogFiles => MapLogs(base.ClsStore_.ClsLogFiles);
+        private string _clsLogFileSelectedItem;
+        private ObservableCollection<string> _clsLogMonth;
+        private ObservableCollection<ClsLogFileLine> _clsLogLines;
+        private bool _isClsMonthEnabled;
+        private string _clsLogMonthSelectedItem;
 
-        public ClsLogViewModel(IServiceProvider serviceProvider): base(serviceProvider) 
+        public ObservableCollection<string> ClsLogFiles => MapLogs(base.ClsStore_.ClsLogFiles);
+        public string ClsLogFileSelectedItem
         {
+            get => _clsLogFileSelectedItem;
+            set
+            {
+                _clsLogFileSelectedItem = value;
+                OnPropertyChanged(nameof(ClsLogFileSelectedItem));
+
+                MapLogYear();
+            }
+        }
+
+        public ObservableCollection<string> ClsLogMonth
+        {
+            get => _clsLogMonth;
+            set
+            {
+                _clsLogMonth = value;
+                OnPropertyChanged(nameof(ClsLogMonth));
+            }
+        }
+        public string ClsLogMonthSelectedItem
+        {
+            get => _clsLogMonthSelectedItem;
+            set
+            {
+                _clsLogMonthSelectedItem = value;
+                
+                MapLogMonth();
+            }
+        }
+        public bool IsClsMonthEnabled
+        {
+            get => _isClsMonthEnabled;
+            set
+            {
+                _isClsMonthEnabled = value;
+                OnPropertyChanged(nameof(IsClsMonthEnabled));
+            }
+        }
+        public ObservableCollection<ClsLogFileLine> ClsLogLines
+        {
+            get => _clsLogLines;
+            set
+            {
+                _clsLogLines = value;
+                OnPropertyChanged(nameof(ClsLogLines));
+            }
+        }
+
+        public ClsLogViewModel(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            _isClsMonthEnabled = true;
+            _clsLogMonth = new ObservableCollection<string>();
+            this.ClsLogFileSelectedItem = "Alle";
 
             SetGlobals();
         }
 
-        private ObservableCollection<ClsLogFileLine> MapLogs(IEnumerable<IEnumerable<ClsLogFileLine>> logs)
+        private ObservableCollection<string> MapLogs(IEnumerable<IEnumerable<ClsLogFileLineType>> logs)
         {
-            var result = new ObservableCollection<ClsLogFileLine>();
+            var result = new List<string>();
 
-            foreach (IEnumerable<ClsLogFileLine> log in logs)
+            foreach (IEnumerable<ClsLogFileLineType> log in logs)
             {
-                foreach (ClsLogFileLine line in log)
+
+                foreach (ClsLogFileLineType line in log)
                 {
-                    result.Add(line);
-                } 
+                    if (result.Contains(line.Year))
+                        continue;
+
+                    result.Add(line.Year);
+                }
             }
 
-            return new ObservableCollection<ClsLogFileLine>(result.Reverse().ToList());
+            result.Add("Alle");
+
+            return new ObservableCollection<string>(new ObservableCollection<string>(result).Reverse().ToList());
+        }
+        private void MapLogYear()
+        {
+            _isClsMonthEnabled = true;
+            var storeLogFiles = base.ClsStore_.ClsLogFiles;
+
+            if (_clsLogFileSelectedItem.Equals("Alle"))
+            {
+                _isClsMonthEnabled = false;
+                _clsLogMonthSelectedItem = null;
+
+                var result = new ObservableCollection<ClsLogFileLine>();
+
+                foreach (IEnumerable<ClsLogFileLineType> log in storeLogFiles)
+                {
+                    foreach (ClsLogFileLineType line in log)
+                    {
+                        result.Add(new ClsLogFileLine(line));
+                    }
+                }
+
+                _clsLogLines = new ObservableCollection<ClsLogFileLine>(new ObservableCollection<ClsLogFileLine>(result).Reverse().ToList());
+                OnPropertyChanged(nameof(ClsLogLines));
+            }
+            else
+            {
+                _clsLogMonth.Clear();
+
+                var resu = new List<string>();
+                
+
+                foreach (IEnumerable<ClsLogFileLineType> log in storeLogFiles)
+                {
+
+                    var fu = log.Select(x => new ClsLogFileLine(x)).ToList();
+                    foreach (ClsLogFileLine line in fu)
+                    {
+                        if (line.Year == _clsLogFileSelectedItem)
+                        {
+                            resu.Add(line.Month);
+                            break;
+                        }
+                    }
+                    
+                }
+
+                resu.Sort();
+                resu.Reverse();
+                resu.Add("Alle");
+                resu.Reverse();
+
+                _clsLogMonth = new ObservableCollection<string>(resu);
+                this.ClsLogMonthSelectedItem = _clsLogMonth.First();
+            }
+
+            OnPropertyChanged(nameof(ClsLogMonth));
+            OnPropertyChanged(nameof(ClsLogMonthSelectedItem));
+            OnPropertyChanged(nameof(IsClsMonthEnabled));
+        }
+        private void MapLogMonth()
+        {
+            if (!string.IsNullOrEmpty(_clsLogMonthSelectedItem) && _clsLogMonthSelectedItem.Equals("Alle"))
+            {
+                _clsLogLines.Clear();
+
+                var result = new ObservableCollection<ClsLogFileLine>();
+
+                foreach (IEnumerable<ClsLogFileLineType> log in base.ClsStore_.ClsLogFiles)
+                {
+
+                    var fu = log.Select(x => new ClsLogFileLine(x)).ToList();
+                    foreach (ClsLogFileLine line in fu)
+                    {
+                        if (line.Month == _clsLogMonthSelectedItem)
+                        {
+                            result.Add(line);
+                        }
+                    }
+                }
+
+                _clsLogLines = new ObservableCollection<ClsLogFileLine>(new ObservableCollection<ClsLogFileLine>(result).Reverse().ToList());
+                OnPropertyChanged(nameof(ClsLogLines));
+            }
         }
         public override void SetGlobals()
         {
@@ -46,5 +195,7 @@ namespace ClsFusionViewer.ViewModels
             IoC.Helper.GetScopedService<InterActionServices>(base.ServiceProvider_)?
                 .SetStatusBarInfoText($"CLS Log-Einträge gefunden: {this.ClsLogFiles.Count}");
         }
+
+
     }
 }
